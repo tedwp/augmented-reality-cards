83be24f52c19b825f8967b61b0fb7e447c7387e3
reaveth <reaveth@gmail.com>
2011-07-10 12:58:47 +0000

    getFPS() added, Video streaming fixed (ending)

--- Cards/TestUI/TestUI.cpp
+++ Cards/TestUI/TestUI.cpp
@@ -10,26 +10,13 @@ int main(int argc, _TCHAR* argv[])
 {
 	int key = 0;
 	Initialize();
-	time(&start);
 
 	while(key!='q') 
 	{
 	//FOR FPS ONLY
-	  time(&end);
-      ++counter;
-      sec = difftime (end, start); 
-	  if(sec == 5)
-	  {
-		  printf("FPS = %.2f\n", FPS);
-		  counter= 0;
-		  start = start+4;
-	  }
-      FPS = counter / sec;
-
-
+		GetFPS();
 	
 		Detect();
-		ShowFrame();
 		key = cvWaitKey(1);
     }
 	Finalize();
