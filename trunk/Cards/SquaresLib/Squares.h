e275194b321807a3ccff7c2f5c4f66e94047011a
reaveth@gmail.com <reaveth@gmail.com@a7962c10-df37-d51c-b962-b3ae934f9a73>
2011-07-10 12:04:14 +0000

    Added documents folder for MasterThesis.docx Added .psd marker templates Added video file Added video streaming possibility.

--- trunk/Cards/SquaresLib/Squares.h
+++ trunk/Cards/SquaresLib/Squares.h
@@ -14,6 +14,7 @@ double sec;
 extern "C" __declspec(dllexport) double GetFPS();
 
 CvCapture		*capture;
+CvCapture		*vidCapture;
 IplImage		*frame;
 IplImage		*gray;
 IplImage		*thresh;
@@ -21,8 +22,9 @@ IplImage		*threshCont;
 IplImage		*imgProc;
 IplImage		*imgMatchResult;
 CvMemStorage	*storage;
-IplImage* imgResult = cvCreateImage(cvSize(1, 1),  IPL_DEPTH_32F, 1);
+IplImage		*imgResult = cvCreateImage(cvSize(1, 1),  IPL_DEPTH_32F, 1);
 
+IplImage		*vidFrame;
 IplImage		*imgDisplay;
 IplImage		*imgTemplate0;
 IplImage		*imgTemplate1;
@@ -32,27 +34,31 @@ IplImage		*imgTemplate3;
 int				count = 4;
 int				markerSide = 100;
 CvSize			markerSize = cvSize(markerSide,markerSide);
+CvSize			testSize = cvSize(300,300);
 double			maxVal;
 int				markerRotation;
+int				returnMarkerRotation;
 CvPoint			finalSquare[4];
 CvSeq			*contours;
 CvSeq			*contoursResult;
 CvSeq			*squares;
 bool			show;
+bool			displayMode;
 
 // Externals //
 extern "C" __declspec(dllexport) bool Initialize();
 extern "C" __declspec(dllexport) void Detect();
 extern "C" __declspec(dllexport) IplImage* GetFrame();
+extern "C" __declspec(dllexport) IplImage* GetVideoFrame();
 extern "C" __declspec(dllexport) void ShowFrame();
 extern "C" __declspec(dllexport) bool Finalize();
-
 
 // Internals//
 void Threshold(IplImage * input);
 void LoadTemplates();
 void Rotate(IplImage * input, float angle, IplImage * out, CvSize SizeRotated);
 void FindContours(IplImage * in, int approx);
-void DrawSquares( IplImage* input, CvSeq* squares );
+void DetectSquares( IplImage* input, CvSeq* squares );
 double MatchMarkers( IplImage* input);
-void visualize(IplImage* input, IplImage* imgDisplay2, CvPoint pt[4]);
+void VisualizeSquares(IplImage* input,int angle, IplImage* display);
+void Visualize(IplImage* input, IplImage* copyinput2, CvPoint pt[4]);
